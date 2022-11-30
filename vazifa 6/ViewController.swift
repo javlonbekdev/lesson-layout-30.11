4b8be886d3b91ac4ab8b93075dec1f1b05f1c811
vazifa 3 <muminovdev@gmail.com>
2022-11-30 05:43:27 +0500

    Initial Commit

---
+++ vazifa 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vazifa 6
+//
+//  Created by Mac on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
